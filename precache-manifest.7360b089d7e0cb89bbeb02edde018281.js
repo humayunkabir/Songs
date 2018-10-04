51b5f6c080356349d943d9ebe62e0c07e351e060
Humayun Kabir <humayunkabir.cep@gmail.com>
2018-10-05 03:13:37 +0600

    Updates

---
+++ precache-manifest.7360b089d7e0cb89bbeb02edde018281.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "031961cc5e2f3f9d6d58",
+    "url": "/static/js/main.031961cc.chunk.js"
+  },
+  {
+    "revision": "c1ee0bdcc1fd317d402d",
+    "url": "/static/js/1.c1ee0bdc.chunk.js"
+  },
+  {
+    "revision": "13dc4f3d9fbb721dae93fea1e0429e27",
+    "url": "/index.html"
+  }
+];
