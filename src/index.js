3143bfe9c136b247070a17a9aad8919541268d64
Humayun Kabir <humayunkabir.cep@gmail.com>
2018-10-22 00:37:03 +0600

    DB added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Songs from './Components/Songs/Songs';
+import App from './App';
 
-ReactDOM.render(<Songs />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
